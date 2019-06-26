756f8f8ca4ce6a32a5fbed416c8d00c333d3af31
Kinght <1903847462@qq.com>
2019-06-26 19:46:52 +0800

    "forth commit"

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 //Java版HelloWorld启动~
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World1!");
     }
 }
